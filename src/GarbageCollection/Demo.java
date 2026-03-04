4dec099e3ec79fbfc4b663b9c4446243b1637ea7
65shrey49 <65shreyasyalamalli49@gmail>
2026-03-04 10:14:47 +0530

    comment

--- src/GarbageCollection/Demo.java
+++ src/GarbageCollection/Demo.java
@@ -4,6 +4,13 @@ public class Demo {
     public void finalize(){
         System.out.println("Garbage collected");
     }
-    public static main(String)
+
+    public static void main(String[] args) {
+        for (int i=0; i<5000; i++){
+            Demo obj = new Demo();
+            obj = null;
+        }
+        System.gc();    // calling garbage collector
+    }
 }
 
